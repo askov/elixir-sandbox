31f38681d7f3c143f529b5ea189208bf5609fe25
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-08-17 22:06:21 +0700

    conditions

--- lib/drop.ex
+++ lib/drop.ex
@@ -9,19 +9,27 @@ defmodule Drop do
   import :math, only: [sqrt: 1]
 
   @spec fall_velocity_old(number()) :: float()
-  def fall_velocity_old(distance, gravity \\ 9.8) do
+  def fall_velocity(planemo, distance) do
+    gravity = case planemo do
+      :earth when distance >=0 -> 9.8
+      :moon  when distance >=0 -> 1.6
+      :mars  when distance >=0 -> 3.71
+    end
     sqrt(2 * gravity * distance)
   end
-  def fall_velocity({planemo, distance}) when distance >= 0 do
-    fall_velocity(planemo, distance)
-  end
-  defp fall_velocity(:earth, distance) do
-    sqrt(2 * 9.8 * distance)
-  end
-  defp fall_velocity(:moon, distance) do
-    sqrt(2 * 1.6 * distance)
-  end
-  defp fall_velocity(:mars, distance) do
-    sqrt(2 * 3.71 * distance)
-  end
+  # def fall_velocity_old(distance, gravity \\ 9.8) do
+  #   sqrt(2 * gravity * distance)
+  # end
+  # def fall_velocity({planemo, distance}) when distance >= 0 do
+  #   fall_velocity(planemo, distance)
+  # end
+  # defp fall_velocity(:earth, distance) do
+  #   sqrt(2 * 9.8 * distance)
+  # end
+  # defp fall_velocity(:moon, distance) do
+  #   sqrt(2 * 1.6 * distance)
+  # end
+  # defp fall_velocity(:mars, distance) do
+  #   sqrt(2 * 3.71 * distance)
+  # end
 end
